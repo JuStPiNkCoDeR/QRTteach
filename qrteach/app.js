85718f1cbf4f137a6119eaa365c2f9ff82e41331
JuStPiNkCoDeR <44189902+JuStPiNkCoDeR@users.noreply.github.com>
2018-10-17 20:54:21 +0600

    My default express app
    Signed-off-by: pinkcoder <sasha.los.0148@gmail.com>

---
+++ qrteach/app.js
@@ -0,0 +1,63 @@
+let express = require('express');
+let path = require('path');
+let favicon = require('static-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let formidable = require('express-formidable');
+let conf = require('./config');
+//var bodyParser = require('body-parser');
+
+let routes = require('./routes/index');
+let users = require('./routes/users');
+
+let app = express();
+
+// view engine setup
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(formidable(conf.get('formidable:options')));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
